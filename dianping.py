5cca47f8e98f91a7c20993037a0764c7c9060ae4
guanghua.zhu <guanghua.zhu@dianping.com>
2014-03-02 23:51:38 +0800

    dianping check in

--- dianping.py
+++ dianping.py
@@ -3,18 +3,36 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait # available since 2.4.0
 from selenium.webdriver.support import expected_conditions as EC # available since 2.26.0
 
-# Create a new instance of the Firefox driver
+def doCheckIn(account):
+	inputElement = driver.find_element_by_css_selector(".J_signbtn>a")
+	print inputElement.text
+	inputElement.click()
+	print account + ":" + inputElement.text
+
+
+def checkIn(account, dper):
+	driver.add_cookie({'name':'dper', 'value':dper, 'path':'/'})
+	driver.get("http://www.dianping.com/shanghai")
+	# find the element that's name attribute is q (the google search box)
+	try:
+		doCheckIn(account)
+		pass
+	except Exception, e:
+		print e
+		pass
+	else:
+		pass
+	finally:
+		pass
+
+def checkByCode():
+	with open('dp.code.txt') as codeTxt:
+	    for line in codeTxt:
+	        nos = line.rstrip().split(' ')
+	        account, dper = nos[0], nos[1]
+	        checkIn(account, dper)
+
 driver = webdriver.Firefox()
-
-# go to the google home page
 driver.get("http://www.dianping.com")
-#driver.add_cookie({'name':'dper', 'value':'72c352db67f3e7e7a582cc4f3d9e5ad0bc1f58dc71cc900728c0b5294d67c79a', 'path':'/'})
-driver.add_cookie({'name':'dper', 'value':'414ff6c94ce01d9a9a4bf7f46ac3c8df1cb2289ee288b9ebf08166956add8433', 'path':'/'})
-driver.get("http://www.dianping.com/shanghai")
-
-# find the element that's name attribute is q (the google search box)
-inputElement = driver.find_element_by_css_selector(".J_signbtn>a")
-print inputElement.text
-inputElement.click()
-print inputElement.text
+checkByCode()
 driver.quit()
--- etao.py
+++ etao.py
@@ -13,7 +13,7 @@ driver.get("http://login.etao.com")
 
 # find the element that's name attribute is q (the google search box)
 driver.switch_to_frame(0)
-element = WebDriverWait(driver, 10, 4).until(EC.presence_of_element_located((By.ID, "TPL_username_1")))
+element = WebDriverWait(driver, 10, 1).until(EC.presence_of_element_located((By.ID, "TPL_username_1")))
 
 pwd = os.path.split(os.path.realpath(__file__))[0]
 username = ''
@@ -25,7 +25,7 @@ with open(os.path.join(pwd, './etao.code.txt')) as auth_txt:
 element.send_keys(username)
 driver.find_element_by_id("TPL_password_1").send_keys(password)
 driver.find_element_by_id("J_SubmitStatic").click()
-element = WebDriverWait(driver, 10, 3).until(EC.presence_of_element_located((By.CLASS_NAME, "ci_receive")))
+element = WebDriverWait(driver, 10, 1).until(EC.presence_of_element_located((By.CLASS_NAME, "ci_receive")))
 print element.text
 element.click()
 driver.quit()
